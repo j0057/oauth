71742e239760e48e59f308e1a6a9582dff30f675
Joost Molenaar <j.j.molenaar@gmail.com>
2014-01-09 19:16:27 +0100

    Update setup.py to make use of setuptools-metadata

--- setup.py
+++ setup.py
@@ -1,31 +1,26 @@
 #!/usr/bin/env python2.7
 
 repo_names = ['xhttp','xmlist']
-package_names = ['requests']
+dist_names = ['requests']
 static_dirs = ['conf', 'static']
 
-import sys
-if len(sys.argv) == 2 and sys.argv[1] == '--install-requires':
-    print '\n'.join(package_names + repo_names)
-elif len(sys.argv) == 2 and sys.argv[1] == '--repo-names':
-    print '\n'.join(repo_names)
-elif len(sys.argv) == 2 and sys.argv[1] == '--package-names':
-    print '\n'.join(package_names)
-elif len(sys.argv) == 2 and sys.argv[1] == '--static-dirs':
-    print '\n'.join(static_dirs)
-else:
-    import os
-    import setuptools
-    setuptools.setup(
-        author='Joost Molenaar',
-        author_email='j.j.molenaar@gmail.com',
-        name='oauth',
-        packages=['oauth'],
-        url='https://github.com/j0057/oauth',
-        version='0.1.0',
-        data_files=[ (root, map(lambda f: root + '/' + f, files))
-                     for src_dir in static_dirs
-                     for (root, dirs, files) in os.walk(src_dir) ],
-        install_requires=package_names + repo_names
-    )
+import os
+from setuptools import setup
+
+setup(
+    author='Joost Molenaar',
+    author_email='j.j.molenaar@gmail.com',
+    name='oauth',
+    packages=['oauth'],
+    url='https://github.com/j0057/oauth',
+    version='0.1.0',
+    data_files=[ (root, map(lambda f: root + '/' + f, files))
+                 for src_dir in static_dirs
+                 for (root, dirs, files) in os.walk(src_dir) ],
+    install_requires=dist_names+repo_names,
+    custom_metadata={
+        'x_repo_names': repo_names,
+        'x_dist_names': dist_names,
+        'x_static_dirs': static_dirs
+    })
 
